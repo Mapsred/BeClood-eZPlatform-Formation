0b25203aa2c2bfeed0000840383d88fdfebbd428
François Mathieu <francois.mathieu@beclood.com>
2017-03-07 17:47:11 +0100

    Add ezpublish_legacy

---
+++ ezpublish_legacy/settings/siteaccess/admin/site.ini.append.php
@@ -0,0 +1,31 @@
+<?php /* #?ini charset="utf-8"?
+
+[SiteSettings]
+DefaultPage=content/view/full/2
+SiteURL=ezplatform_formation.loc/admin
+
+[SiteAccessSettings]
+ShowHiddenNodes=true
+RequireUserLogin=true
+RelatedSiteAccessList[]
+RelatedSiteAccessList[]=admin
+
+[Session]
+ActivityTimeout=86400
+CookieTimeout=86400
+
+[RegionalSettings]
+TranslationSA[]
+TranslationSA[fre]=Fre
+Locale=fre-FR
+ContentObjectLocale=fre-FR
+ShowUntranslatedObjects=disabled
+SiteLanguageList[]=fre-FR
+TextTranslation=enabled
+
+[DesignSettings]
+SiteDesign=admin2
+AdditionalSiteDesignList[]=admin
+AdditionalSiteDesignList[]=base
+AdditionalSiteDesignList[]=standard
+*/ ?>
